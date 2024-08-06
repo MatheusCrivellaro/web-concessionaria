f43527a98bdf400912b93a2cd8b97425ceba0cfe
MatheusCrivellaro <128638676+MatheusCrivellaro@users.noreply.github.com>
2024-08-06 11:31:23 -0300

    commit initial

---
+++ web-concessionaria/src/main.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import App from './App.tsx';
+import './index.css';
+
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
