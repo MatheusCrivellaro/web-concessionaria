3d0fe403503de14704f74aa8eda3e79c64050503
MatheusCrivellaro <128638676+MatheusCrivellaro@users.noreply.github.com>
2024-08-07 09:53:16 -0300

    nav creation

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,6 +7,23 @@ const NavBar = () => {
                 <h1 className="title">Logo</h1>
                 <input className='search-nav' type="text" placeholder="Pesquise por veículo ou marca..." />
             </div>
+            <div className='optionals-div-nav'>
+                <button className='active-button-nav button-nav'>
+                    <p className='text-active-button-nav text-button-nav'>Início</p>
+                </button>
+                <button className='deactivated-button-nav button-nav'>
+                    <p className='text-deactivated-button-nav text-button-nav'>Início</p>
+                </button>
+                <button className='deactivated-button-nav button-nav'>
+                    <p className='text-deactivated-button-nav text-button-nav'>Início</p>
+                </button>
+                <button className='deactivated-button-nav button-nav'>
+                    <p className='text-deactivated-button-nav text-button-nav'>Início</p>
+                </button>
+                <button className='deactivated-button-nav button-nav'>
+                    <p className='text-deactivated-button-nav text-button-nav'>Início</p>
+                </button>
+            </div>
         </nav>
     )
 }
