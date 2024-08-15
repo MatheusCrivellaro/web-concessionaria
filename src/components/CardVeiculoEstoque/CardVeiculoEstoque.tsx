893ad0f8b01f7d96acc5b38e2781d58371874b56
MatheusCrivellaro <matheuscriv@gmail.com>
2024-08-15 18:53:56 -0300

    commit

--- src/components/CardVeiculoEstoque/CardVeiculoEstoque.tsx
+++ src/components/CardVeiculoEstoque/CardVeiculoEstoque.tsx
@@ -5,16 +5,6 @@ import imagemGenerica from '../../images/img-categoria.png'
 import {Vehicle} from "../../interfaces/Vehicle.ts";
 import {MdOutlineCalendarMonth} from "react-icons/md";
 import {SlSpeedometer} from "react-icons/sl";
-import honda from "../../images/marcas/Honda_Logo.svg.png";
-import fiat from "../../images/marcas/fiat-logo-2-1.png";
-import mercedesBenz from "../../images/marcas/mercedes-benz-logo-8.png";
-import jeep from "../../images/marcas/jeep_logo_icon_145814.png";
-import hyundai from "../../images/marcas/Hyundai-Logo-1990.png";
-import chevrolet from "../../images/marcas/Chevrolet-Logo.png";
-import volkswagen from "../../images/marcas/Volkswagen-Logo.png";
-import renault from "../../images/marcas/pngimg.com - renault_PNG45.png";
-import ford from "../../images/marcas/Ford-Logo.png";
-import toyota from "../../images/marcas/Toyota-Simbolo.png";
 import peugeot from "../../images/marcas/16.png";
 
 type props = {
@@ -30,31 +20,6 @@ const trataFoto = (image: string | undefined) => {
 
 const CardVeiculoEstoque = ({ image, veiculo }:props) => {
 
-    const marcasImg = [
-        {brandName: 'honda', imageSrc: honda},
-        {brandName: 'fiat', imageSrc: fiat},
-        {brandName: 'mercedes-benz', imageSrc: mercedesBenz},
-        {brandName: 'jeep', imageSrc: jeep},
-        {brandName: 'hyundai', imageSrc: hyundai},
-        {brandName: 'chevrolet', imageSrc: chevrolet},
-        {brandName: 'volkswagen', imageSrc: volkswagen},
-        {brandName: 'renault', imageSrc: renault},
-        {brandName: 'ford', imageSrc: ford},
-        {brandName: 'toyota', imageSrc: toyota},
-        {brandName: 'peugeot', imageSrc: peugeot},
-        {brandName: '', imageSrc: imagemGenerica},
-    ];
-
-    const getImgMarca = (marca: string) => {
-        let imgMarca = marcasImg[11]
-        marcasImg.forEach(value => {
-            if (marca.toLowerCase() === value.brandName) {
-                imgMarca = value
-            }
-        })
-        return imgMarca
-    }
-
     return (
         <div className="col-3 card-veiculo-div-estoque">
             <div className="card-veiculo-estoque shadow">
@@ -62,7 +27,7 @@ const CardVeiculoEstoque = ({ image, veiculo }:props) => {
                     <img src={trataFoto(image)} alt="" className="image-card-veiculo-estoque"/>
                 </div>
                 <div>
-                    <h4><img src={getImgMarca(veiculo.marca).imageSrc} alt="" className="img-logo-card-veiculo"/> <span>{veiculo.marca + " "}</span>{veiculo.modelo}</h4>
+                    <h4><img src={peugeot} alt="" className="img-logo-card-veiculo"/> <span>{veiculo.marca + " "}</span>{veiculo.modelo}</h4>
                     <h2>{veiculo.versao}</h2>
                     <div className="ano-km-div-card-veiculo-estoque">
                         <p><MdOutlineCalendarMonth className="icon-card-veiculos-estoque"/>{veiculo.anoFabricacao + "/" + veiculo.anoModelo}</p>
--- src/components/CarouselCategorias/CarouselCategorias.tsx
+++ src/components/CarouselCategorias/CarouselCategorias.tsx
@@ -1,17 +1,6 @@
 import './CarouselCategorias.css'
 import CategoriaCard from "../CategoriaCard/CategoriaCard.tsx";
-import honda from "../../images/marcas/Honda_Logo.svg.png";
-import fiat from "../../images/marcas/fiat-logo-2-1.png";
-import mercedesBenz from "../../images/marcas/mercedes-benz-logo-8.png";
-import jeep from "../../images/marcas/jeep_logo_icon_145814.png";
-import hyundai from "../../images/marcas/Hyundai-Logo-1990.png";
-import chevrolet from "../../images/marcas/Chevrolet-Logo.png";
-import volkswagen from "../../images/marcas/Volkswagen-Logo.png";
-import renault from "../../images/marcas/pngimg.com - renault_PNG45.png";
-import ford from "../../images/marcas/Ford-Logo.png";
-import toyota from "../../images/marcas/Toyota-Simbolo.png";
 import peugeot from "../../images/marcas/16.png";
-import imagemGenerica from "../../images/img-categoria.png";
 
 type props = {
     marcas: string[],
@@ -20,31 +9,6 @@ type props = {
 
 const CarouselCategorias = ({ marcas, handleSelectedMarca }: props) => {
 
-    const marcasImg = [
-        {brandName: 'honda', imageSrc: honda},
-        {brandName: 'fiat', imageSrc: fiat},
-        {brandName: 'mercedes-benz', imageSrc: mercedesBenz},
-        {brandName: 'jeep', imageSrc: jeep},
-        {brandName: 'hyundai', imageSrc: hyundai},
-        {brandName: 'chevrolet', imageSrc: chevrolet},
-        {brandName: 'volkswagen', imageSrc: volkswagen},
-        {brandName: 'renault', imageSrc: renault},
-        {brandName: 'ford', imageSrc: ford},
-        {brandName: 'toyota', imageSrc: toyota},
-        {brandName: 'peugeot', imageSrc: peugeot},
-        {brandName: '', imageSrc: imagemGenerica},
-    ];
-
-    const getImgMarca = (marca: string) => {
-        let imgMarca = marcasImg[11]
-        marcasImg.forEach(value => {
-            if (marca.toLowerCase() === value.brandName) {
-                imgMarca = value
-            }
-        })
-        return imgMarca
-    }
-
     return (
         <div className="categorias-div-inicio">
             <h1>Marcas</h1>
@@ -52,7 +16,7 @@ const CarouselCategorias = ({ marcas, handleSelectedMarca }: props) => {
                 {
                     marcas.map((i, index) =>
                         <CategoriaCard
-                            image={getImgMarca(i).imageSrc}
+                            image={peugeot}
                             title={i}
                             key={index + "categoria"}
                             handleSelectedMarca={handleSelectedMarca}
