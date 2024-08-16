3aada1bf562dae4a1f7031111d3b05da79dc12d4
MatheusCrivellaro <matheuscriv@gmail.com>
2024-08-15 23:44:34 -0300

    adicionando um carrossel de banners e um para cada card

--- src/components/CarouselBanner/CarouselBanner.tsx
+++ src/components/CarouselBanner/CarouselBanner.tsx
@@ -1,17 +1,14 @@
 import './CarouselBanner.css'
 
-import banner1 from "../../images/7fe01d06-dd18-463e-be8e-371369438f38.jpeg"
-import banner2 from "../../images/a22ca6d1-cc43-4afb-9662-bd71bc9ed10f.jpg"
-
 const CarouselBanner = () => {
     return (
         <div id="carouselExample" className="carousel slide carousel-banner">
             <div className="carousel-inner carousel-div-banner">
                 <div className="carousel-item active div-banners-carousel">
-                    <img src={banner1} alt=""/>
+                    <img src="https://resized-images.autoconf.com.br/1920x450/filters:format(webp)/site/64/7fe01d06-dd18-463e-be8e-371369438f38.png" alt=""/>
                 </div>
                 <div className="carousel-item div-banners-carousel">
-                    <img src={banner2} alt=""/>
+                    <img src="https://resized-images.autoconf.com.br/1920x450/filters:format(webp)/site/64/a22ca6d1-cc43-4afb-9662-bd71bc9ed10f.png" alt=""/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
