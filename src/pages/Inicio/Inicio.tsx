d02d4f59c2f95b7b8b219137d4bf966aea67a265
MatheusCrivellaro <matheuscriv@gmail.com>
2024-08-21 02:28:49 -0300

    adicionando um carrossel de banners e um para cada card

--- src/pages/Inicio/Inicio.tsx
+++ src/pages/Inicio/Inicio.tsx
@@ -17,11 +17,13 @@ import useCollects from "../../hooks/useCollects.tsx";
 import CarouselCategorias from "../../components/CarouselCategorias/CarouselCategorias.tsx";
 import CardVeiculoEstoque from "../../components/CardVeiculoEstoque/CardVeiculoEstoque.tsx";
 import CarouselBanner from "../../components/CarouselBanner/CarouselBanner.tsx";
+import {useEffect, useState} from "react";
 
 const Inicio = () => {
 
     const { data } = useGetStock();
     const { marcas  } = useCollects(data)
+    const [destaques, setDestaques] = useState<Vehicle[]>()
 
     const navigate = useNavigate();
 
@@ -29,29 +31,13 @@ const Inicio = () => {
         navigate('/veiculos/#veiculos', { state: { marcaSelecionada: selectedMarca } });
     };
 
+    useEffect(() => {
+        setDestaques(data?.slice(0, 8))
+    }, [data]);
+
     return (
         <div className="inicio">
             <CarouselBanner />
-            {/*<div className="banner-div-inicio">*/}
-            {/*    <div className="text-banner-div-inicio">*/}
-            {/*        <div className="text-banner-inicio">*/}
-            {/*            <h1>Seu próximo carro <span>está aqui!</span></h1>*/}
-            {/*            <h2>*/}
-            {/*                Adquira agora mesmo seu veículo dos sonhos com <span>condições especiais!</span>*/}
-            {/*            </h2>*/}
-            {/*            <h2>Veja agora mesmo nossos veículos em destaque:</h2>*/}
-            {/*            <div className="div-buttons-banner-inicio">*/}
-            {/*                <Link to="/financiamento" className="button-banner-inicio button-banner-grey">*/}
-            {/*                    Fazer um financiamento*/}
-            {/*                </Link>*/}
-            {/*                <a href="/#veiculos-destaque" className="button-banner-inicio button-banner-blue">*/}
-            {/*                    Ver veículos em destaque*/}
-            {/*                </a>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*    <img src={imageBannerinicio} alt="" />*/}
-            {/*</div>*/}
             <div className="informations-div-inicio row">
                 <InformationCard title="Carros novos e semi-novos" description="Nós proporcionamos para você condições especiais para poder adquirir o veículo dos seus sonhos! " image={iconInformation1}/>
                 <InformationCard title="Compra de veículos" description="Compramos seu veículo sem burocracia, com agilidade na análise e preço justo." image={iconInformation2}/>
@@ -65,7 +51,7 @@ const Inicio = () => {
             <div className="veiculos-destaque-div-inicio" id="veiculos-destaque">
                 <h1>Veículos em destaque</h1>
                 <div className="veiculos-destaque-cards-div-inicio row">
-                    {data?.slice(0, 8).map((i: Vehicle, index) =>
+                    {destaques?.map((i: Vehicle, index) =>
                         <CardVeiculoEstoque veiculo={i} key={"carro" + index}/>
                     )}
                 </div>
